134637d16bf664b421c4f14ad71ba03ac22deb8f
Tomasz Wierzbowski <tomaswierzbowski@gmail.com>
2023-01-16 18:15:35 +0100

    video adjustments

--- wp-content/themes/listinghive/page.php
+++ wp-content/themes/listinghive/page.php
@@ -4,6 +4,70 @@ the_post();
 get_header();
 ?>
 <div class="page__text">
+
+
+<?php
+if ( is_front_page() ){
+?>
+<style>
+
+@media only screen and (min-width: 850px) {
+.count {
+	float: left;
+	width: 33%
+}
+}
+
+@media only screen and (max-width: 849px) {
+.count {
+	width: 100%;
+	text-align: center;
+}
+}
+
+.count-container {
+	font-size: 50px;
+	height: 240px;
+	color: rgba(15,23,39,.65);
+	line-height: 1.5;
+	text-align: center;
+}
+
+@media only screen and (max-width: 849px) {
+.count-container {
+	margin-bottom: 110px;
+}
+}
+
+
+</style>
+<div class="count-container">
+<?php
+$count_listings = wp_count_posts( 'hp_listing' )->publish;
+$count_vendors = wp_count_posts( 'hp_vendor' )->publish;
+
+$args = array();
+$comments = get_comments($args);
+$count_comments = count($comments);
+?>
+<div class="count">
+<?php echo $count_listings . ' '; ?>
+<span style="font-size: 24px;">Ogłoszenia</span> 
+</div>
+<div class="count">
+<?php echo $count_vendors . ' '; ?>
+<span style="font-size: 24px;">Ogłoszeniodawcy</span>
+</div>
+<div class="count">
+<?php echo $count_comments . ' '; ?>
+<span style="font-size: 24px;">Komentarze</span>
+</div>
+<div style="clear: both;"></div>
+</div>
+<?php
+}
+?>
+
 	<?php the_content( null, true ); ?>
 </div>
 <?php
--- wp-content/themes/listinghive/templates/page/page-header.php
+++ wp-content/themes/listinghive/templates/page/page-header.php
@@ -1,4 +1,33 @@
 <section class="header-hero <?php echo esc_attr( $class ); ?>" <?php if ( get_theme_mod( 'header_image_parallax' ) ) : ?>data-component="parallax"<?php endif; ?>>
+<style>
+.video-container {
+z-index: -100;
+width:100%;
+height:100%;
+overflow:hidden;
+position:absolute;
+top:0;
+left:0;
+}
+#video-bg{
+  width:100%;
+ 
+}
+	.header-navbar{
+		background-color:white;
+	}
+	.header-hero{
+		background-color:rgba(0,0,0,0)!important;
+		background-image:none!important;
+	}	
+</style>
+
+<!-- <section class="header-hero">			 -->
+	 <div class="video-container">
+      <video autoplay loop muted id="video-bg">
+        <source src="//test2.makeitwork.com.pl/wp-content/uploads/2022/11/filmik-online-video-cutter.com-1.mp4" type="video/mp4">
+      </video>
+    </div>
 	<div class="header-hero__content">
 		<div class="container">
 			<?php echo $content; ?>
